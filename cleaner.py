ff865882e1b61bd56e4fec93e5d8efc43e01b16e
LaChina Barnes <lachinabarnes@LaChinasMacBook.lan>
2026-04-18 09:16:02 -0400

    first commit

---
+++ cleaner.py
@@ -0,0 +1,9 @@
+def clean_data(data):
+    """
+    TODO: Implement your "clean_heartrate_data" function from TLAB #1 & #2
+    within this module. Note that this code will be *slightly* different
+    from your original function.
+    """
+    clean_data = []
+    ...
+    return clean_data
