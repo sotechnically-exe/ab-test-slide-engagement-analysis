865a4a8d065aef9018e97ba208ca28a81329897f
La'China Barnes <lachinabarbarnes@gmail.com>
2026-04-19 10:39:02 -0400

    implement the clean_data() function

--- cleaner.py
+++ cleaner.py
@@ -3,7 +3,15 @@ def clean_data(data):
     TODO: Implement your "clean_heartrate_data" function from TLAB #1 & #2
     within this module. Note that this code will be *slightly* different
     from your original function.
+
+    We need to skip the first row [0]
     """
     clean_data = []
-    ...
+    for row in data:
+        if row != "minutes\n":
+            clean_data.append(float(row.strip()))
+        
     return clean_data
+
+    #list slicing?
+    
